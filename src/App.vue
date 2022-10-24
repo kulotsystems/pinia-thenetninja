8d4d1f9ff97382c393ddeb31caf49527e942b5ff
kulotsystems <kulotsystems@gmail.com>
2022-10-24 08:32:10 +0800

    Lesson 2: Project Setup.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
-<script>
-
-</script>
-
 <template>
-
+    <main>
+        <!-- heading -->
+        <header>
+            <img src="./assets/pinia-logo.svg" alt="Pinia Logo">
+            <h1>Pinia Tasks</h1>
+        </header>
+    </main>
 </template>
+
+<script>
+    export default {
+
+    }
+</script>
 
 <style scoped>
 
